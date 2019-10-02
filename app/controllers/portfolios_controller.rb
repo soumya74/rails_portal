93306e6e8027630e37abd2ffd254deb08816700a
soumya74 <soumya74@gmail.com>
2019-10-03 00:20:28 +0530

    refactored strong_parameter in portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,8 +17,7 @@ class PortfoliosController < ApplicationController
     end
 
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
-          technologies_attributes: [:name]))
+        @portfolio_item = Portfolio.new(portfolio_params)
 
         respond_to do |format|
           if @portfolio_item.save
@@ -38,7 +37,7 @@ class PortfoliosController < ApplicationController
     def update
         @portfolio_item = Portfolio.find(params[:id])
         respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          if @portfolio_item.update(portfolio_params)
             format.html { redirect_to portfolios_path, notice: 'portfolio_item was successfully updated.' }
             format.json { render :show, status: :ok, location: @portfolio_item }
           else
@@ -59,6 +58,15 @@ class PortfoliosController < ApplicationController
         respond_to do |format|
             format.html { redirect_to portfolios_url, notice: "Record was removed"}
         end
+    end
+
+private
+    def portfolio_params
+      params.require(:portfolio).permit(:title,
+                                        :subtitle,
+                                        :body,
+                                        technologies_attributes: [:name]
+                                        )
     end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module DevcampPortfolio
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # config.action_controller.permit_all_parameters = true
   end
 end
