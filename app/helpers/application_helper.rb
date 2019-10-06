43025a5cc9117f046698e8831c34e6eced7e2638
soumya74 <soumya74@gmail.com>
2019-10-06 21:54:49 +0530

    Styling Home pages and Edit page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(style)
     if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
     else
-      "<h1>".html_safe + (link_to "Login", new_user_session_path) + " / " +
-      (link_to "Register", new_user_registration_path) + "</h1>".html_safe
+      (link_to "Login", new_user_session_path, class: style) + " " +
+      (link_to "Register", new_user_registration_path, class: style)
     end
   end
 
