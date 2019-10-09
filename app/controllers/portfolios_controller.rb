fb4abd5822b73811008a745ce83fdc53f69af34c
soumya74 <soumya74@gmail.com>
2019-10-09 15:08:22 +0530

    Fixed edit_profile technology bug, added edit_login_account button

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,7 +18,6 @@ class PortfoliosController < ApplicationController
 
     def new
         @portfolio_item = Portfolio.new
-        3.times {@portfolio_item.technologies.build}
     end
 
     def create
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def login_helper(style = '')
     if !current_user.is_a?(GuestUser)
+      (link_to "Edit", edit_user_registration_path, class: style)  + "&ensp;".html_safe +
       (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
     else
       (link_to "Login", new_user_session_path, class: style) + "&ensp;".html_safe +
